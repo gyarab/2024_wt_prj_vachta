4d1dd5f12d8e8170604a63d35174e1d6b181dff7
Oliver Vachta <oliver.vachta.s@gyarab.cz>
2025-02-16 20:45:23 +0100

    np

---
+++ exchange.py
@@ -0,0 +1,23 @@
+import httpx
+
+print("Obchodni prevodnik")
+
+url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt?date=13.02.2025"
+
+r = httpx.get(url)
+#print(r.text)
+
+in_array = r.text.split("\n")
+
+#print(in_array)
+
+for line in in_array:
+    if "|EUR|" in line:
+        print(line)
+        
+castka = input("Zadej castku v CZK: ")
+result = int(castka) / 25
+print(f"To je v EUR:  {result}")
+castaka2 = input("Zadejte castku v CZK: ")
+result2 = int(castaka2) * 25
+print(f"To je v CYK:  {result2}")
