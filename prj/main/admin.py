464dce80a6147ef502a08b3ea95462447f3efbfe
Oliver Vachta <oliver.vachta.s@gyarab.cz>
2025-03-28 08:54:56 +0100

    d

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Model, Produkt
+
+class ObuvAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "year","produkt"]
+    
+class ProduktAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "year","produkt"]
 
 # Register your models here.
+admin.site.register(Model, ObuvAdmin)
+admin.site.register(Produkt, ProduktAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+class Model(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, default="")
+    produkt = models.ForeignKey("Produkt", on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f"Model <{self.id}> {self.name}"
 
-# Create your models here.
+class Produkt(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(blank=True, null=True)
+    birth_year = models.IntegerField(blank=True, default="")
+    description = models.TextField(blank=True, default="")
+    
+    def __str__(self):
+        return f"Produkt <{self.id}> {self.name}"
