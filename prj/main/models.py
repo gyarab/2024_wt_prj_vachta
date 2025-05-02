9890ef8523c0cf6f4bbea5ae45313d3ad1ce8d59
Oliver Vachta <oliver.vachta.s@gyarab.cz>
2025-05-02 10:33:02 +0200

    p

--- prj/main/models.py
+++ prj/main/models.py
@@ -1,17 +1,23 @@
 from django.db import models
+
 class VKosiku(models.Model):
     jmeno = models.CharField(max_length=300)
     rok = models.IntegerField(blank=True, null=True)
     popis = models.TextField(blank=True, default="")
-    produkt = models.ForeignKey("Produkt", on_delete=models.SET_NULL, null=True)
-    
+    produkt = models.ForeignKey(
+        "Produkt", on_delete=models.SET_NULL, null=True
+    )
+
     def __str__(self):
         return f"VKosiku <{self.id}> {self.jmeno} {self.rok} {self.popis} {self.produkt}"
+
 
 class Produkt(models.Model):
     jmeno = models.CharField(max_length=300)
     popis = models.TextField(blank=True, default="")
-    obrazek = models.ImageField(upload_to='produkty/', blank=True, null=True)
+    obrazek = models.ImageField(
+        upload_to='produkty/', blank=True, null=True
+    )
     cena = models.DecimalField(max_digits=10, decimal_places=2)
     barva = models.CharField(max_length=100, blank=True)
     velikost = models.CharField(max_length=50, blank=True)
--- prj/main/views.py
+++ prj/main/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from main.models import Produkt
 
 def get_homepage(request):
+    produkt = Produkt.objects.all().order_by("jmeno")[:10]
+    
     context = {
         "svatek": "Libor",
-        "jmeno": Produkt.objects.all()
+        "jmeno": Produkt.objects.all().order_by("jmeno")[:10]
      }
 
-    return render(request,"main/base.html", context)
+    if request.GET.get("search"):
+        print("SEARCH", request.GET.get("search"))
+        produkt = Produkt.filter(title_icontains=request.GET.get("search")).order_by("jmeno")[:10]
+    
+    return render(
+        request, "main/base.html", context
+    )
